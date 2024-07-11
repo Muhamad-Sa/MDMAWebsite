071f8c3c5a96d2051ec36e96734ecdd1c7068a3e
Ahmed Tarek <ahmedttaarek@gmail.com>
2024-07-11 10:34:45 +0300

    Last edit in homepage to make it responsive

--- Homepage/Homepagescript.js
+++ Homepage/Homepagescript.js
@@ -27,3 +27,14 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    burgerMenu.addEventListener('click', () => {
+        navLinks.classList.toggle('open');
+    });
+});
