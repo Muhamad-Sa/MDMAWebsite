3885487a9cf2fb5d665dbc9f36d0de29fcc14e6c
Muhamad Samir <mohamedsamiir7@gmail.com>
2024-07-18 12:00:43 +0300

    remvoed redundant code

--- Script/menPageScript.js
+++ Script/menPageScript.js
@@ -41,23 +41,6 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
-
-  function passItemData(event, itemId) {
-    event.preventDefault();
-
-    fetch("data.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const itemData = data[itemId]; // Get item data by ID
-
-        // Store item data in sessionStorage for use in item.html
-        sessionStorage.setItem("itemData", JSON.stringify(itemData));
-
-        // Navigate to item.html
-        window.location.href = "item.html";
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }
 });
 
 function showSection(sectionId) {
@@ -75,47 +58,3 @@ function showSection(sectionId) {
 
 // Initially show the first section
 document.getElementById("newcollcetion").style.display = "grid";
-
-// search by name in search bar
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("data.json")
-    .then((response) => response.json())
-    .then((items) => {
-      const searchInput = document.getElementById("searchInput");
-      const searchResults = document.getElementById("searchResults");
-
-      searchInput.addEventListener("input", function () {
-        const query = this.value.toLowerCase().trim();
-
-        if (query.length > 0) {
-          const filteredItems = items.filter((item) =>
-            item.name.toLowerCase().includes(query)
-          );
-          displaySearchResults(filteredItems);
-        } else {
-          searchResults.style.display = "none";
-        }
-      });
-
-      function displaySearchResults(results) {
-        searchResults.innerHTML = "";
-        if (results.length > 0) {
-          results.forEach((item) => {
-            const resultItem = document.createElement("a");
-            resultItem.href = `item.html?id=${item.id}`; // Link to item page
-            resultItem.innerHTML = `<img src="${item.images[0]}" alt="${item.name}"> ${item.name}`;
-            resultItem.addEventListener("click", function (event) {
-              event.preventDefault();
-              sessionStorage.setItem("itemData", JSON.stringify(item));
-              window.location.href = this.href;
-            });
-            searchResults.appendChild(resultItem);
-          });
-          searchResults.style.display = "block";
-        } else {
-          searchResults.style.display = "none";
-        }
-      }
-    })
-    .catch((error) => console.error("Error fetching items:", error));
-});
