f43016a286f75c5e4779655ce121acca799ee959
Muhamad Samir <mohamedsamiir7@gmail.com>
2024-07-18 06:11:52 +0300

    removed unused code

--- Script/Homepagescript.js
+++ Script/Homepagescript.js
@@ -2,9 +2,3 @@ document.getElementById("scrollButton").addEventListener("click", function () {
       const productsSection = document.getElementById("categories");
       productsSection.scrollIntoView({ behavior: "smooth" });
       });
-
-
-
-
-
-
