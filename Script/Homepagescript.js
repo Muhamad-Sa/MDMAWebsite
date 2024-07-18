21fc71fc7ca6048f68f64aa670c97f8a382c8aa9
Muhamad Samir <mohamedsamiir7@gmail.com>
2024-07-18 04:49:57 +0300

    Deleted some unusable code

--- Script/Homepagescript.js
+++ Script/Homepagescript.js
@@ -1,40 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const categories = document.querySelectorAll(".category");
-
-  // Toggle description visibility on click
-  categories.forEach((category) => {
-    const description = category.querySelector("p");
-    category.addEventListener("click", () => {
-      const isActive = category.classList.contains("active");
-
-      // Reset all categories
-      categories.forEach((cat) => {
-        cat.classList.remove("active"); // Remove active class
-        const desc = cat.querySelector("p");
-        if (desc !== description) {
-          desc.style.display = "none"; // Hide other descriptions
-        }
+document.getElementById("scrollButton").addEventListener("click", function () {
+      const productsSection = document.getElementById("categories");
+      productsSection.scrollIntoView({ behavior: "smooth" });
       });
-
-      // Toggle active class and description visibility
-      if (!isActive) {
-        category.classList.add("active"); // Add active class to clicked category
-        description.style.display = "block"; // Show description
-      } else {
-        category.classList.remove("active"); // Remove active class on second click
-        description.style.display = "none"; // Hide description
-      }
-    });
-
-    //shop now scroll button
-    document
-      .getElementById("scrollButton")
-      .addEventListener("click", function () {
-        const productsSection = document.getElementById("categories");
-        productsSection.scrollIntoView({ behavior: "smooth" });
-      });
-  });
-});
 
 
 
